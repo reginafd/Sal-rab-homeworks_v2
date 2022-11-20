42ef990c1b2c38e8a10985395afbdbf27d5791cc
reginafd <siamka.f@gmail.com>
2022-11-20 17:06:19 +0300

    fix_1 task_2

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,9 +5,18 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
+    let shippingSum;                    
 
-    // если productsSum равно 0,
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+
+        shippingSum = 0;
+    } else {
+        shippingSum = shippingPrice;
+    }
+      }  
+
+    // создайте переменную shippingSum
+    // если productsSum равно 0
     // то shippingSum присвоить значение 0
 
     // если productsSum Больше или равна freeShippingMinSum,
